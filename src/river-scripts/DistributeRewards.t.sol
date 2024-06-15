b1922531a0bbf733cf26ed2ab791d223794f8e99
John Terzis <john@hntlabs.com>
2024-06-15 16:00:00 -0700

    updates to include RewardsDistribution

--- src/river-scripts/DistributeRewards.t.sol
+++ src/river-scripts/DistributeRewards.t.sol
@@ -7,6 +7,7 @@ import {LlamaCore} from "src/LlamaCore.sol";
 import {LlamaAccount} from "src/accounts/LlamaAccount.sol";
 import {LlamaExecutor} from "src/LlamaExecutor.sol";
 import {IOptimismMintableERC20} from "src/interfaces/IOptimismMintableERC20.sol";
+import {IRewardsDistribution} from "src/interfaces/IRewardsDistribution.sol";
 import {DistributeRewardsScriptBase, DistributeRewardsScriptBaseSepolia} from "src/llama-scripts/DistributeRewardsScript.sol";
 import {LlamaTestSetup} from "test/utils/LlamaTestSetup.sol";
 
@@ -46,15 +47,21 @@ contract DistributeRewardsBaseSepolia is DistributeRewardsTestSetupBaseSepolia {
 contract DistributeRewardsTestSetupBase is LlamaTestSetup {
   address public constant RIVER_EXECUTOR = 0x63217D4c321CC02Ed306cB3843309184D347667B;
   address public constant RIVER_CORE = 0xA547373eB2b3c93AdeB27ec72133Fb7B92F70F7f;
-  uint256 public constant BLOCK_NUMBER = 15_848_234;
+  uint256 public constant BLOCK_NUMBER = 15_851_201;
   /// @dev The Treasury Llama account address.
   address public constant RIVER_TREASURY = 0x8ee48C016b932A69779A25133b53F0fFf66C85C0;
   /// @dev The RVR ERC20 token address.
   IOptimismMintableERC20 internal constant RVR_TOKEN = IOptimismMintableERC20(0x9172852305F32819469bf38A3772f29361d7b768);
+  /// Base Registry
+  address internal constant REGISTRY_DIAMOND = 0x7c0422b31401C936172C897802CF0373B35B7698;
+  IRewardsDistribution internal constant REWARDS_DISTRIBUTION = IRewardsDistribution(REGISTRY_DIAMOND);
   /// @dev The Base Bridge.
   address internal constant BRIDGE_BASE = 0x4200000000000000000000000000000000000010;
   uint256 internal constant PERIOD_AMOUNT = 30_769_230;
   DistributeRewardsScriptBase public rewardsScript;
+
+  /// operators
+  address internal constant FRAMEWORK = 0x09285F179a9bA06CEBA12DeCd1755Ac6942A8cf4;
 
 
   function setUp() public override {
@@ -76,6 +83,12 @@ contract DistributeRewardsBase is DistributeRewardsTestSetupBase {
     assertEq(block.number, BLOCK_NUMBER);
     // assert treasury balance
     assertEq(RVR_TOKEN.balanceOf(RIVER_TREASURY), PERIOD_AMOUNT + 1);
+    // check period distribution amount
+    uint256 periodDistributionAmount = REWARDS_DISTRIBUTION.getPeriodDistributionAmount();
+    console.log("periodDistributionAmount: ", periodDistributionAmount);
+    // check an operators claimable amount before distribution
+    uint256 frameworkClaimableAmt = REWARDS_DISTRIBUTION.getClaimableAmountForOperator(FRAMEWORK);
+    console.log("frameworkClaimableAmt: ", frameworkClaimableAmt);
     // Authorize script
     vm.prank(address(RIVER_EXECUTOR));
     LlamaCore(RIVER_CORE).setScriptAuthorization(address(rewardsScript), true);
@@ -85,5 +98,8 @@ contract DistributeRewardsBase is DistributeRewardsTestSetupBase {
     LlamaExecutor(RIVER_EXECUTOR).execute(
       address(rewardsScript), true, abi.encodeWithSignature("distributeOperatorRewards()")
     );
+    // get claimable amount after distributing rewards
+    uint256 frameworkClaimableAmtPost = REWARDS_DISTRIBUTION.getClaimableAmountForOperator(FRAMEWORK);
+    console.log("frameworkClaimableAmt: ", frameworkClaimableAmtPost);
   }
 }
